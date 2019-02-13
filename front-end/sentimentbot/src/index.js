68b6fe6c5724cdefaf369645a8267f70c7d92a7b
MichelleOk <umbralcompendium@gmail.com>
2019-02-13 16:09:11 -0700

    commented out the store, since it is not set up yet, hoping this allows deployment again

--- front-end/sentimentbot/src/index.js
+++ front-end/sentimentbot/src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 // const store = createStore(whatever, applyMiddleware(thunk, logger))
 
-ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider><Router><App /></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
