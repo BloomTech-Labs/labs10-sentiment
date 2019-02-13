bbe33ddecbc06c9a514e534a43a85ce3f1c9da6c
MichelleOk <umbralcompendium@gmail.com>
2019-02-13 14:57:13 -0700

    added homepage navigation, awaiting links to other pages

---
+++ front-end/sentimentbot/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Home() {
+    return (
+        <div className="homeContent">
+        <h1 className="welcomeText">Welcome to Mood</h1>
+        <Link to="">Enter</Link>
+        </div>
+    )
+}
+export default Home;
