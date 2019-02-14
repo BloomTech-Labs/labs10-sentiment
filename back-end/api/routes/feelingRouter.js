7e199b9aa94fab6fdfea060fc374da447bd89032
Zatara-Wrought <zatara.forever@gmail.com>
2019-02-14 15:51:52 -0800

    Feeling Router

--- back-end/api/routes/feelingRouter.js
+++ back-end/api/routes/feelingRouter.js
@@ -1,2 +1,17 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("../database/dbConfig");
+
+router.post("/", (req, res) => {
+  const info = req.body;
+  db.insert(info)
+    .into("feelings")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "failed to post new team member", err });
+    });
+});
+
+module.exports = router;
