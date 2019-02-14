71330c516885d39c060e14dbff2b46cb457bd809
dyiar <austin.hl.blake@gmail.com>
2019-02-14 15:28:07 -0800

    added get endpoint for manager router

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const configureMiddleware = require("./middleware/middleware.js");
-// const managerRouter = require("./routes/managerRouter.js");
+const managerRouter = require("./routes/managerRouter.js");
 // const teamMemberRouter = require("./routes/teamMemberRouter.js");
 // const teamRouter = require("./routes/teamRouter.js");
 // const surveyRouter = require("./routes/surveyRouter.js");
@@ -10,7 +10,7 @@ const server = express();
 
 configureMiddleware(server);
 
-// server.use('/api/managers', managerRouter);
+server.use('/api/managers', managerRouter);
 // server.use('/api/team_members', teamMemberRouter);
 // server.use('/api/teams', teamRouter);
 // server.use('/api/surveys', surveyRouter);
