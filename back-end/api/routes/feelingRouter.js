571a5ccff281c824a41d24b8d329aeabb6d5d4d0
Zatara-Wrought <zatara.forever@gmail.com>
2019-02-14 16:02:59 -0800

    Added FeelingRouter

--- back-end/api/routes/feelingRouter.js
+++ back-end/api/routes/feelingRouter.js
@@ -1,2 +1,19 @@
-// const express = require("express");
-// const router = express.Router();
+feelingrouter.js;
+
+const express = require("express");
+const router = express.Router();
+const db = require("../database/dbConfig");
+
+router.post("/", (req, res) => {
+  const info = req.body;
+  db.insert(info)
+    .into("feelings")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "failed to post new team member", err });
+    });
+});
+
+module.exports = router;
