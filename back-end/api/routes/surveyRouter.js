aa0b3e538f610fdb67a03cdc7e19370dcda2ebca
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-17 19:12:46 -0500

    fixed routes

--- back-end/api/routes/surveyRouter.js
+++ back-end/api/routes/surveyRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/dbConfig.js");
+const db = require("../database/helpers/surveysDb");
 
 const {
   postSuccess,
--- back-end/api/routes/survey_feelingsRouter.js
+++ back-end/api/routes/survey_feelingsRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/dbConfig");
+const db = require("../database/helpers/surveysFeelingsDb");
 
 const {
   postSuccess,
