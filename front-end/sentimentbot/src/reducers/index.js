9a855502c594a909d756d0223f2789089354490a
MichelleOk <umbralcompendium@gmail.com>
2019-02-19 09:47:05 -0700

    fixed actiona nd reducer import, export, and endpoints for survey

--- front-end/sentimentbot/src/reducers/index.js
+++ front-end/sentimentbot/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { feelingsReducer } from './feelingsReducer';
 import { managersReducers } from './managersReducers';
+import { surveyReducer } from './surveyReducer';
 
 export default combineReducers ({
     feelingsReducer,
-    managersReducers
+    managersReducers,
+    surveyReducer,
 })
