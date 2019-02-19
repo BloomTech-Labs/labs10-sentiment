cc139637e2c696ce1880ab5e2c355986b2f10dda
MichelleOk <umbralcompendium@gmail.com>
2019-02-19 10:27:39 -0700

    cleaned up actions

--- front-end/sentimentbot/src/actions/index.js
+++ front-end/sentimentbot/src/actions/index.js
@@ -1,10 +1,6 @@
 
 export * from "./feelings.js";
 export * from "./managers";
-
 export * from "./teams";
-
-export * from './feelings.js';
-export * from './managers';
 export * from './survey.js';
 
