36e0d8cfcbd2681681ea1e2663707577d7c0eed6
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-20 16:13:18 -0500

    added to my profile

--- front-end/sentimentbot/src/Components/myProfile/myProfile.js
+++ front-end/sentimentbot/src/Components/myProfile/myProfile.js
@@ -8,57 +8,151 @@ import {
 import axios from "axios";
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      view: "",
+      name: ""
+    };
+  }
   componentDidMount() {
     this.props.getSingleTeamMembers(localStorage.getItem("email"));
     this.props.getTeamMembers();
-    const code = this.props.match.params.code;
-    console.log(code);
-    if (code) {
-      this.fetchAuth(code);
-    }
+
+    // const code = this.props.match.params.code;
+    // console.log(code);
+    // if (code) {
+    //   this.fetchAuth(code);
   }
 
-  componentDidUpdate(prev) {
-    const code = this.props.match.params.code;
-    console.log(code);
-    if (code !== prev.match.params.code) {
-      this.fetchAuth(code);
-    }
+  // this.props.teamMembers.length !== prevProps.teamMembers.length
+
+  componentDidUpdate(prevProps) {
+    // if (this.props.singleTeamMembers !== prevProps.singleTeamMembers) {
+    //   this.props.getSingleTeamMembers(localStorage.getItem("email"));
+    //   console.log(this.props.singleTeamMembers);
+    // }
+    // this.props.getSingleTeamMembers(localStorage.getItem("email"));
+    // this.props.getTeamMembers();
+    // const code = this.props.match.params.code;
+    // console.log(code);
+    // if (code !== prev.match.params.code) {
+    //   this.fetchAuth(code);
+    // }
   }
 
-  fetchAuth = code => {
-    axios
-      .post(
-        `https://slack.com/api/oauth.access`,
-        `client_id=555765331446.554661112789&client_secret=65618f3ce7feca293e1abae74cae7afc&code=${code}&redirect_uri=https://sentimentbot.netlify.com/&single_channel=false`
-      )
-      .then(response => {
-        // this.setState(() => ({ movie: response.data }));
-        console.log("response", response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  // fetchAuth = code => {
+  //   axios
+  //     .post(
+  //       `https://slack.com/api/oauth.access`,
+  //       `client_id=555765331446.554661112789&client_secret=65618f3ce7feca293e1abae74cae7afc&code=${code}&redirect_uri=https://sentimentbot.netlify.com/home&single_channel=false`
+  //     )
+  //     .then(response => {
+  //       // this.setState(() => ({ movie: response.data }));
+  //       console.log("response", response);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // };
+
+  createTeam = event => {
+    event.preventDefault();
+
+    // const postEndPoint = `https://botsentiment.herokuapp.com/api/teams`;
+    // let postInfo = {};
+    // axios
+    //   .post()
+    //   .then()
+    //   .catch();
+
+    // const single = this.props.singleTeamMembers[0];
+
+    // const endpoint = `https://botsentiment.herokuapp.com/api/team_members/:${
+    //   single.id
+    // }`;
+    // const changes = {};
+    // axios
+    //   .put(endpoint, changes)
+    //   .then(response => {
+    //     // this.setState(() => ({ movie: response.data }));
+    //     console.log("response", response);
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
   };
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  // submitHandler = event => {
+  //   event.preventDefault();
+  //   this.props.addTeamMembers(this.state);
+  //   this.setState({
+  //     ...this.state,
+  //     name: this
+  //   });
+  //   history.push("/profile");
+  // };
+
   render() {
-    return (
-      <div>
-        <a href="https://slack.com/oauth/authorize?scope=commands,bot&client_id=553324377632.554405336645">
-          <img
-            alt="Add to Slack"
-            height="40"
-            width="139"
-            src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-          />
-        </a>
+    const view = this.state.view;
+    console.log(view);
+    if (view === "") {
+      return (
         <div>
-          <button>Join a Team</button>
-          <button>Create a Team</button>
+          <a href="https://slack.com/oauth/authorize?scope=commands,bot&client_id=553324377632.554405336645">
+            <img
+              alt="Add to Slack"
+              height="40"
+              width="139"
+              src="https://platform.slack-edge.com/img/add_to_slack.png"
+              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            />
+          </a>
+          <div>
+            {/* <p>{this.props.singleTeamMembers[0].id}</p> */}
+            <button
+              onClick={()=>{this.setState({
+                ...this.state,
+                view: "join"
+              })}}
+            >
+              Join a Team
+            </button>
+            <button
+              onClick={()=>{this.setState({
+                ...this.state,
+                view: "create"
+              })}}
+            >
+              Create a Team
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else if (view === "create") {
+      return (
+        <div>
+          <form onSubmit={this.submitHandler} autoComplete="nope">
+            <input
+              autoComplete="off"
+              type="text"
+              onChange={this.handleChange}
+              name="name"
+              placeholder="Add Team Name"
+              value={this.state.name}
+            />
+            <button onClick={this.createTeam}>Submit Team Tile</button>
+          </form>
+        </div>
+      );
+    } else if (view === "join") {
+    }
   }
 }
 
