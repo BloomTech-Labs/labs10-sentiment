2a33d4f56983dbb36e2963bdd6aa8478435cf432
dyiar <austin.hl.blake@gmail.com>
2019-02-26 14:38:15 -0700

    fixed some things before pull request

--- back-end/api/routes/surveyRouter.js
+++ back-end/api/routes/surveyRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database/helpers/surveysDb");
 const managersdb = require("../database/helpers/managersDb");
-const feelingsdb = require('../database/helpers/surveysFeelingsDb');
+// const feelingsdb = require('../database/helpers/surveysFeelingsDb');
 
 const {
   postSuccess,
