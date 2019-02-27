af1227979e1971f560c6322f60c564237b13bfc8
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-27 16:53:30 -0500

    test scheduler 3

--- back-end/api/routes/surveyRouter.js
+++ back-end/api/routes/surveyRouter.js
@@ -64,7 +64,7 @@ const surveyScheduler = (timeInfo, postInfo) => {
   }
 
   if (timeInfo.dailyWeeklyMonthly === "daily") {
-    exTime = `23 ${hour} * * *`; //////////////////////////////////////////////////////////////////////////////
+    exTime = `56 ${hour} * * *`; //////////////////////////////////////////////////////////////////////////////
   } else if (timeInfo.dailyWeeklyMonthly === "weekly") {
     exTime = `0 ${hour} * * 5`;
   } else if (timeInfo.dailyWeeklyMonthly === "monthly") {
