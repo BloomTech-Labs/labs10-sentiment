1ff483d2ce440492078b5b4414637b3de7dfab14
Thomas Claydon <tomclaydon102@gmail.com>
2019-02-27 19:03:35 -0500

    modified feeling migration 4

--- back-end/api/routes/surveyRouter.js
+++ back-end/api/routes/surveyRouter.js
@@ -67,7 +67,7 @@ const surveyScheduler = (timeInfo, postInfo) => {
   }
 
   if (timeInfo.dailyWeeklyMonthly === "daily") {
-    exTime = `3 ${hour} * * *`; ///////////////////////////////////////////////////////////////////////////////
+    exTime = `6 ${hour} * * *`; ///////////////////////////////////////////////////////////////////////////////
   } else if (timeInfo.dailyWeeklyMonthly === "weekly") {
     exTime = `0 ${hour} * * 5`;
   } else if (timeInfo.dailyWeeklyMonthly === "monthly") {
