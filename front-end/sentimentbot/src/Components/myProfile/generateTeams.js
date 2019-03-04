6e50700b93c6becede9f7cbf320b56b63e2c9ace
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-03-04 14:08:27 -0500

    Fixes to changes on files listed

--- front-end/sentimentbot/src/Components/myProfile/generateTeams.js
+++ front-end/sentimentbot/src/Components/myProfile/generateTeams.js
@@ -1,35 +1,27 @@
-import React from 'react'
+import React from "react";
 import { connect } from "react-redux";
-
-import { getSingleTeam } from "../../actions";
-
-import { getFeelings } from "../../actions";
-
-import GenerateList from './generateList'
+import { getSingleTeam } from "../../actions/index";
+import GenerateList from "./generateList";
 
 class GenerateTeams extends React.Component {
-
-  
   render() {
-
-      return (
-        <>
-        <GenerateList history = {this.props.history} />
+    return (
+      <>
+        <GenerateList history={this.props.history} />
         {/* <Select options={surveyList} /> */}
+      </>
+    );
+  }
+}
 
-        </>
-        
-      );
-    }
-  }
-  
-  function mapStateToProps(state) {
-    return {
-      feelings: state.feelingsReducer.feelings
-    };
-  }
-  
-  export default connect(
-    mapStateToProps,
-    { getFeelings }
-  )(GenerateTeams);
+function mapStateToProps(state) {
+  return {
+    singleTeams: state.teamsReducer.singleTeams,
+    teams: state.teamsReducer.teams
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { getSingleTeam }
+)(GenerateTeams);
