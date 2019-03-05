b2708baf2c190aff423cf11e7dbe6ee8e106fb08
MichelleOk <umbralcompendium@gmail.com>
2019-03-05 11:20:25 -0700

    bootstrap nav start

--- front-end/sentimentbot/src/Components/NavBar/NavBar.js
+++ front-end/sentimentbot/src/Components/NavBar/NavBar.js
@@ -1,19 +1,84 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink as Navlink } from 'react-router-dom';
 import Logout from "../login/logout";
 import "./NavBar.css";
 import Auth from "../Auth/auth";
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 const auth = new Auth();
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
   return (
     <>
       {this.props.singleTeamMembers[0].type === "manager" ? (
         <>
+          <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/home">MOOD</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink><Navlink to="/profile">Profile</Navlink></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Navlink to="/teamlist">Team List</Navlink></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Navlink  to="/survey">Survey</Navlink></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Navlink  to="/billing">Billing</Navlink></NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    <NavItem>
+                      <NavLink><Navlink to="/accountset">Account Settings</Navlink></NavLink>
+                    </NavItem>
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
           <Logout auth={auth} />
           <div className="navBar">
             <div className="navButton">
