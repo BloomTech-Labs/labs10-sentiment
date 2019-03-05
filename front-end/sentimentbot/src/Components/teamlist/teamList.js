81a59f5051ce3052e5ec18cf1c8f35539c8d5bff
dyiar <austin.hl.blake@gmail.com>
2019-03-05 10:59:15 -0800

    made small changes

--- front-end/sentimentbot/src/Components/teamlist/teamList.js
+++ front-end/sentimentbot/src/Components/teamlist/teamList.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { connect } from "react-redux";
-import { getTeamsMembers, editTeamMembers } from "../../actions/index";
-import "./teamlist.css";
+import React from 'react';
+import { connect } from 'react-redux';
+import { getTeamsMembers, editTeamMembers } from '../../actions/index';
+import './teamlist.css';
 
 class TeamList extends React.Component {
-  componentWillMount() {
-    this.props.getTeamsMembers(this.props.singleTeamMembers[0].team_id);
-  }
 
-  handleClick(data) {
-    this.props.editTeamMembers(data.id, data);
-    console.log(data.id);
-  }
+    componentWillMount() {
+        this.props.getTeamsMembers(this.props.singleTeamMembers[0].team_id)
+    }
+    
+    handleClick(data) {
+        this.props.editTeamMembers(data.id, data);
+        console.log(data.id)
+        
+    }
 
-  render() {
-    const uri = "https://sentimentbot.netlify.com/authorization";
-    return (
-      <>
-        <div className="container">
-          <p>{this.props.singleTeams[0].name}</p>
-          <a
-            href={`https://slack.com/oauth/authorize?scope=commands,bot&client_id=553324377632.554405336645&redirect_uri=${uri}&state=${
-              this.props.singleTeamMembers[0].id
-            }`}
+    render() {
+        const uri = "https://sentimentbot.netlify.com/authorization";
+        return (
+            <>
+            <div className="container">
+            <p>{this.props.singleTeams[0].name}</p>
+            <a
+            href={`https://slack.com/oauth/authorize?scope=commands,bot&client_id=553324377632.554405336645&redirect_uri=${uri}&state=${this.props.singleTeamMembers[0].id}`}
           >
             <img
               alt="Add to Slack"
@@ -36,45 +36,31 @@ class TeamList extends React.Component {
 
           <h3>Team Members</h3>
           <div className="teamlist-container">
-            {this.props.teamMembers.map(user => {
-              return (
-                <div>
-                  <p>{user.firstName}</p>
-                  <button
-                    onClick={() =>
-                      this.handleClick({
-                        id: user.id,
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        email: user.email,
-                        phone: user.phone,
-                        type: null,
-                        team_id: null
-                      })
-                    }
-                  >
-                    Remove
-                  </button>
-                </div>
-              );
-            })}
+              {this.props.teamMembers.map(user => {
+                  return (
+                  <div>
+                      <p>{user.firstName}</p>
+                      <button onClick={() => this.handleClick({id: user.id, firstName: user.firstName, lastName: user.lastName, email: user.email, phone: user.phone, type: null, team_id: null})}>Remove</button>
+                  </div>
+                    )
+              })}
           </div>
-        </div>
-      </>
-    );
-  }
+          </div>
+            </>
+        )
+    }
 }
 
 function mapStateToProps(state) {
-  return {
-    teamMembers: state.teamsReducer.teamMembers,
-    teams: state.teamsReducer.teams,
-    singleTeams: state.teamsReducer.singleTeams,
-    singleTeamMembers: state.teamMembersReducer.singleTeamMembers
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  { getTeamsMembers, editTeamMembers }
-)(TeamList);
+    return {
+        teamMembers: state.teamsReducer.teamMembers,
+        teams: state.teamsReducer.teams,
+        singleTeams: state.teamsReducer.singleTeams,
+        singleTeamMembers: state.teamMembersReducer.singleTeamMembers
+    };
+  }
+  
+  export default connect(
+    mapStateToProps,
+    { getTeamsMembers, editTeamMembers }
+  )(TeamList);
