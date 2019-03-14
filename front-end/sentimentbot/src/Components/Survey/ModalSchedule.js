40b5aec4aabb8ec20f38571170967da3b0bfef37
Gothic Jellybean <gothicjellybean@Gothics-iMac.local>
2019-03-14 13:05:31 -0600

    minor fixes css

--- front-end/sentimentbot/src/Components/Survey/ModalSchedule.js
+++ front-end/sentimentbot/src/Components/Survey/ModalSchedule.js
@@ -84,16 +84,16 @@ class ModalSchedule extends React.Component {
             <div className="survey-inputbox">
                 <label className="survey-inputlabel">Minute</label>
                 <select id="min" value={this.props.state.min} onChange={this.props.onChangeDropDown}>
-                <option value={0}>0</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
+                <option value={0}>00</option>
+                <option value={1}>01</option>
+                <option value={2}>02</option>
+                <option value={3}>03</option>
+                <option value={4}>04</option>
+                <option value={5}>05</option>
+                <option value={6}>06</option>
+                <option value={7}>07</option>
+                <option value={8}>08</option>
+                <option value={9}>09</option>
                 <option value={10}>10</option>
                 <option value={11}>11</option>
                 <option value={12}>12</option>
