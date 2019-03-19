ec55720d432639fd11805f706d64c5ab31f9a419
Joseph <josephryan96@gmail.com>
2019-03-19 09:16:43 -0700

    Made some changes to the josephModal

--- front-end/sentimentbot/src/Components/CreatorPage/josephModal.js
+++ front-end/sentimentbot/src/Components/CreatorPage/josephModal.js
@@ -4,30 +4,40 @@ import Joseph from "../PNG/JosephBot.png"
 
 
 class JosephModal extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        modal: false,
-      };
-  
-      this.toggle = this.toggle.bind(this);
-    }
-  
-    toggle() {
-      this.setState(prevState => ({
-        modal: !prevState.modal
-      }));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
 
-render() {
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  render() {
     return (
-        <div>
+      <div>
         <Button color="secondary" onClick={this.toggle}>Meet Me!</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Joseph</ModalHeader>
           <ModalBody>
-            <p>Talk about your self!</p>
-            <img src={Joseph} width="300" height="300" alt="joseph" />
+            <div className="joseph-container">
+              <p>Hey there, I'm Joseph.</p>
+              <p>A few of my interests: Software Development, Gaming, Traveling.</p>
+              <p>If you'd like to connect, or learn more about me, follow my links below!</p>
+              <p>Thanks for checking out Sentiment Bot (aka MOOD).</p>
+              <img src={Joseph} width="300" height="300" alt="joseph" />
+              <div className="joseph-link-container">
+                <a className="fab fa-twitter-square fa-3x" href="https://twitter.com/_josepheastman" target="_blank"></a>
+                <a className="fab fa-github-square fa-3x" href="https://github.com/josepheastman" target="_blank"></a>
+                <a className="fab fa-linkedin fa-3x" href="https://www.linkedin.com/in/joseph-eastman/" target="_blank"></a>
+              </div>
+            </div>
           </ModalBody>
           <ModalFooter>
             <Button color="secondary" onClick={this.toggle}>Return</Button>
